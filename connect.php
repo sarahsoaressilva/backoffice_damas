2c551368f7c239b5b30cc17e6bde843818a94701
Sarah Soares <contatodev.sarah@gmail.com>
2022-05-23 19:19:04 -0300

    Concerta JOIN planos

---
+++ connect.php
@@ -0,0 +1,239 @@
+<?php
+
+    $plano_id = 0;
+    $nome = '';
+    $descricao = '';
+    $tipo = '';
+    $valor = 0;
+    $update = false;
+    
+    $servername = "localhost";
+    $username = "root";
+    $password = "dev@22";
+    $database = "damas";
+    
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $database);
+
+    // Método de INSERT 
+    if ( isset($_POST['save']) ) {
+        $nome = $_POST['nome'];
+        $descricao = $_POST['descricao'];
+        $tipo = $_POST['tipo'];
+        $valor = $_POST['valor'];
+        
+        
+        $conn->query("INSERT INTO Planos (nome, descricao, valor, tipo)
+        VALUES ('$nome', '$descricao', $valor, '$tipo') ") or die( $conn->error() );
+        
+        header("location: planos.php");
+        
+    } //Fim INSERT
+    
+    // Método de DELETE 
+    if ( isset($_GET['delete']) ) {
+        $plano_id = $_GET['delete'];
+        
+        $conn->query("DELETE FROM Planos WHERE plano_id=$plano_id") or die( $conn->error() );
+        
+        $conn->query("DELETE FROM Planos_Itens WHERE fk_id_plano=$plano_id") or die( $conn->error() );
+        
+        header("location: planos.php");
+        
+    } //Fim DELETE
+    
+    // Método de EDIT
+    if ( isset($_GET['edit']) ) {
+        $plano_id = $_GET['edit'];
+        $update = true;
+        
+        $result = $conn->query("SELECT * FROM Planos WHERE plano_id=$plano_id") or die( $conn->error() );
+      
+        $row = $result->fetch_array();
+        
+        if( count( array ($result) ) == 1 ) {
+            $plano_id = $row['plano_id'];
+            $nome = $row['nome'];
+            $descricao = $row['descricao'];
+            $valor = $row['valor'];
+            $tipo = $row['tipo'];
+        }
+        
+        header("location: planos.php");
+        
+    }    //Fim EDIT
+        
+    // Método de UPDATE 
+    if ( isset($_POST['update']) ) {
+        $plano_id = $_POST['plano_id'];
+        $nome = $_POST['nome'];
+        $descricao = $_POST['descricao'];
+        $valor = $_POST['valor'];
+        $tipo = $_POST['tipo'];
+            
+        $conn->query("UPDATE Planos SET nome='$nome', descricao='$descricao', tipo='$tipo', valor=$valor WHERE
+        plano_id=$plano_id") or die( $conn->error() );
+
+        header("location: planos.php");
+            
+    } //Fim UPDATE
+    
+    // Ao apertar o botão Ver Itens, essa função é acionada.
+    if ( isset($_GET['itens']) ) {
+      $plano_id = $_GET['itens'];
+
+      // ---------------------------------------- Tabuleiros
+      $imprimir = " 
+        <div class='container'>
+           <h4> Tabuleiro </h4>
+        <div class='row justify-content-center'>
+          
+          <div class='table-resposive'>
+            <table class='table table-striped'>
+              <thead>
+                  <tr>
+                      <th> Tab_Id </th>
+                      <th> Nome do Tabuleiro </th>
+                      <th> Descrição </th>
+                      <th> Valor </th>
+                      <th> Imagem </th>
+                      <th> Plano_id </th>
+                  </tr>
+              </thead>
+      ";
+
+      echo $imprimir;
+
+      // Consulta do Tabuleiro
+      $sql = " SELECT * FROM Tabuleiro WHERE plano_id=$plano_id";
+      $result = $conn->query($sql) or die( $conn->error() );
+
+      // Imprime dados do Tabuleiro
+      echo '<br>';
+      while( $row = $result->fetch_assoc() ) {
+
+        $imprimir = "
+          <tr>
+            <td> $row[tab_id] </td>
+            <td> $row[nome] </td>
+            <td> $row[descricao] </td>
+            <td> $row[valor] </td>
+            <td> <img src='$row[img]' width='100' height='100'> </td>
+            <td> $row[plano_id] </td>
+          </tr>
+        ";
+
+        echo $imprimir;
+      }
+
+      $imprimir = '
+          </table>
+        </div> 
+      </div>
+      ';
+
+      echo $imprimir;
+      
+      // ---------------------------------------- Peças
+      $imprimir = " 
+        <h4> Peças </h4>
+        <div class='row justify-content-center'>
+          
+          <div class='table-resposive'>
+            <table class='table table-striped'>
+              <thead>
+                  <tr>
+                      <th> Peca_Id </th>
+                      <th> Nome da Peça </th>
+                      <th> Descrição </th>
+                      <th> Valor </th>
+                      <th> Imagem </th>
+                      <th> Plano_id </th>
+                  </tr>
+              </thead>
+      ";
+
+      echo $imprimir;
+
+      // Consulta das Peças
+      $sql = " SELECT * FROM Pecas WHERE plano_id=$plano_id";
+      $result = $conn->query($sql) or die( $conn->error() );
+
+      echo '<br>';
+      // Imprime dados do Tabuleiro
+      while( $row = $result->fetch_assoc() ) {
+        $imprimir = "
+          <tr>
+            <td> $row[peca_id] </td>
+            <td> $row[nome] </td>
+            <td> $row[descricao] </td>
+            <td> $row[valor] </td>
+            <td> <img src='$row[img]' width='100' height='100'> </td>
+            <td> $row[plano_id] </td>
+          </tr>
+        ";
+
+        echo $imprimir;
+      }
+
+      $imprimir = '
+          </table>
+        </div> 
+      </div>
+      ';
+
+      echo $imprimir;
+
+      // ---------------------------------------- Icones
+
+      $imprimir = " 
+        <h4> Icones </h4>
+        <div class='row justify-content-center'>
+          
+          <div class='table-resposive'>
+            <table class='table table-striped'>
+              <thead>
+                  <tr>
+                      <th> Icon_Id </th>
+                      <th> Nome do Icone </th>
+                      <th> Descrição </th>
+                      <th> Valor </th>
+                      <th> Imagem </th>
+                      <th> Plano_id </th>
+                  </tr>
+              </thead>
+      ";
+
+      echo $imprimir;
+
+      // Consulta das Icones
+      $sql = " SELECT * FROM Icones WHERE plano_id=$plano_id";
+      $result = $conn->query($sql) or die( $conn->error() );
+
+      echo '<br>';
+
+      // Imprime dados dos Icones
+      while( $row = $result->fetch_assoc() ) {
+        $imprimir = "
+          <tr>
+            <td> $row[icones_id] </td>
+            <td> $row[nome] </td>
+            <td> $row[descricao] </td>
+            <td> $row[valor] </td>
+            <td> <img src='$row[img]' width='100' height='100'> </td>
+            <td> $row[plano_id] </td>
+          </tr>
+        ";
+
+        echo $imprimir;
+      }
+
+      $imprimir = '
+          </table>
+        </div> 
+      </div>
+      ';
+
+      echo $imprimir;
+    } 
+  
---
+++ planos.php
@@ -0,0 +1,215 @@
+<!doctype html>
+<html lang="en">
+        <title>Backoffice</title>
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="style.css">
+    
+    
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <!-- CONEXÃO PHP -->
+    <?php require_once 'connect.php'; ?>  
+
+  </head>
+  <body>
+      
+    <!-- BARRA DE NAVEGAÇÃO -->
+    <nav class="navbar stick-top navbar-expand-lg navbar-dark bg-primary">
+        <a class="navbar-brand" href="#">Back Office</a>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="/"> Home </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/tabuleiro-php/tabuleiro.php"> Tabuleiro </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/peças-php/peças-crud.php"> Peças </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/icones-php/icones.php"> Icones </a>
+                    </li>
+                     <li class="nav-item active">
+                        <a class="nav-link" href="/planos-php/planos.php"> Planos </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/show-db/show-db.php"> Planos_Itens </a>
+                    </li>
+                </ul>
+            </div>
+    </nav>   
+    
+    <br>
+    <br>
+    
+    <!-- FORMULÁRIO -->
+    <div class="container">
+        <div class="row align-items-start">
+            
+        <form action="connect.php" method="POST">
+            <input type='hidden' name='plano_id' value='<?php echo $plano_id ?>' >
+            
+            <div class="form-group">
+                <label> Nome do Plano </label>
+                <input type="text" class="form-control form-control-sm" name="nome" 
+                value="<?php echo $nome; ?>" >
+            </div>
+            
+            <div class="form-group">
+                <label> Descrição </label>
+                <input type="text" class="form-control" name="descricao" 
+                value="<?php echo $descricao; ?>" >
+            </div>
+            
+            <div class="form-group">
+                <label> Valor </label>
+                <input type="number" step='0.01' class="form-control" name="valor" 
+                value="<?php echo $valor; ?>" >
+            </div> 
+            
+            <div class="form-group">
+                <label> Insira o tipo de plano (M = Mensal, A = Anual) </label>
+                <input type="text" class="form-control form-control-sm" name="tipo" 
+                value="<?php echo $tipo; ?>" >
+            </div>
+    
+            <div class="form-group">
+                
+            <?php
+            if($update == true):
+            ?>
+                <button type="submit" name="update" class="btn btn-info"> Editar </button>
+            <?php else: ?>
+                <button type="submit" name="save" class="btn btn-primary"> Salvar </button>
+            <?php endif; ?>
+            
+            </div>
+        </form>
+        </div> 
+    </div>
+    
+    <br>
+    <br>
+ 
+    <!-- AREA DE EXIBIÇÃO DE ITENS -->
+    <div class="container">
+    <?php
+      $conn = new mysqli('localhost', 'root', 
+      'dev@22', 'damas');
+      
+      $result = $conn->query("SELECT * FROM Planos") or die( $conn->error() );
+    ?>
+     <div class="row justify-content-center">
+        <table class="table">
+          <thead>
+            <tr>
+              <th> Id do Plano </th>
+              <th> Nome do Plano </th>
+              <th> Descrição </th>
+              <th> Tipo </th>
+              <th> Valor (R$) </th>
+              <th> Ações </th>
+            </tr>
+          </thead>
+        <?php
+          while($row = $result->fetch_assoc() ): ?>
+          <tr>
+            <td> <?php echo $row['plano_id']; ?> </td>
+            <td> <?php echo $row['nome']; ?> </td>
+            <td> <?php echo $row['descricao']; ?> </td>
+            <td> <?php echo $row['tipo']; ?> </td>
+            <td> <?php echo $row['valor']; ?> </td>
+            <td> 
+                <a href="planos.php?edit=<?php echo $row['plano_id']; ?>" class="btn btn-info" > Editar </a> 
+    
+                <a href="planos.php?delete=<?php echo $row['plano_id']; ?>" class="btn btn-danger"> Deletar </a> 
+                
+                <!-- Button trigger modal -->
+                <?php
+                  $id = $row['plano_id'];
+                ?>
+                
+                <a href="planos.php?itens=<?php echo $id ?>" class="btn btn-outline-primary">
+                Ver Itens 
+                </a>
+            </td>
+          </tr> 
+        <?php endwhile; ?>
+        </table>
+      </div>
+    </div>
+    
+    <!-- CODIGOS JAVASCRIPT DO BOOTSTRAP -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    
+        <!-- Modal Visualizar Itens -->
+        <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+          <div class="modal-dialog" role="document">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel"> Itens do Plano </h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+              <div class="modal-body">
+                
+                <!-- Visualizar Tabuleiros -->
+                <?php
+                  $conn = new mysqli('localhost', 'root', 
+                  'dev@22', 'damas');
+
+                    $sql = " SELECT * FROM Tabuleiro ";
+                    $result = $conn->query( $sql ) or die( $conn->error() );
+                ?>    
+                
+                <div class="row justify-content-center">
+                    
+                <h5> Tabuleiros </h5>    
+                <!-- CABEÇALHO TABULEIRO -->
+                <table class="table">
+                  <thead>
+                    <tr>
+                      <th> Id do Item </th>
+                      <th> Nome do Item </th>
+                      <th> Descrição </th>
+                      <th> Valor (R$) </th>
+                    </tr>
+                 </thead>
+                 
+                <!-- WHILE PARA EXIBIR AS LINHAS DO BD DO TABULEIRO -->
+                <?php
+                while($row = $result->fetch_assoc() ): ?>
+                    <tr>
+                        <td> <?php echo $row['tab_id']; ?> </td>
+                        <td> <?php echo $row['nome']; ?> </td>
+                        <td> <?php echo $row['descricao']; ?> </td>
+                        <td> <?php echo $row['valor']; ?> </td>
+                    </tr>
+                <?php endwhile; ?>
+                
+                </table>
+                
+                <!-- Visualizar Peças -->
+                <h4> Peças </h4>
+                
+            </div>
+              
+              <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal"> Fechar </button>
+                <button type="button" class="btn btn-primary"> Salvar  </button>
+              </div>
+              
+            </div>
+          </div>
+        </div>
+    
+  </body>
+</html>
